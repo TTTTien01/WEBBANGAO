8ad6a3b15971c165d5dc6a7641200f07b6c7472c
LAPTOP-154T03IB\HP <tranthithuytien2203@gmail.com>
2021-11-02 22:23:09 +0700

    update_code

--- WebBanGao/WebBanGao/Layout.Master.cs
+++ WebBanGao/WebBanGao/Layout.Master.cs
@@ -9,9 +9,44 @@ namespace WebBanGao
 {
     public partial class Layout : System.Web.UI.MasterPage
     {
+        QLBanGaoDataContext db = new QLBanGaoDataContext();
+        public static List<DanhmucSp> ListDanhmucSp = new List<DanhmucSp>();
+        public static List<DanhmucTinTuc> ListDanhmucTinTuc = new List<DanhmucTinTuc>();
+        public static List<DanhMucKhuyenmai> ListDanhMucKhuyenmai = new List<DanhMucKhuyenmai>();
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            LoadDanhmucSp();
+            LoadDanhTinTuc();
+            LoadDanhMucKhuyenmai();
+        }
+        public void LoadDanhmucSp()
+        {
+            var sp = (from q in db.DanhmucSps
+                     select q);
+            if (sp != null && sp.Count() > 0)
+            {
+                ListDanhmucSp = sp.ToList();
+            }
+        }
+        public void LoadDanhTinTuc()
+        {
+            var tt = (from p in db.DanhmucTinTucs
+                      select p);
+            if (tt != null && tt.Count() > 0)
+            {
+                ListDanhmucTinTuc = tt.ToList();
+            }
+        }
 
+        public void LoadDanhMucKhuyenmai()
+        {
+            var km = (from q in db.DanhMucKhuyenmais
+                      select q);
+            if (km != null && km.Count() > 0)
+            {
+                ListDanhMucKhuyenmai = km.ToList();
+            }
         }
     }
 }
